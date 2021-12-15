f46a4c4d5ef91b47d29ab554b6e8fc97cae373e0
Franchesco <fr-tb@hotmail.com>
2021-12-15 13:29:48 -0500

    proyect slice

---
+++ slice.go
@@ -0,0 +1,74 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	arr := [4]int{}
+
+	fmt.Println("*********************")
+
+	fmt.Println("Enter your 4 Numbers ")
+
+	fmt.Scan(&arr[0])
+	fmt.Scan(&arr[1])
+	fmt.Scan(&arr[2])
+	fmt.Scan(&arr[3])
+
+	slic := arr[0:3]
+
+	fmt.Println("*********************")
+
+	fmt.Println("Do you want Print 3 or 4 Numbers")
+
+	var answer int
+
+	fmt.Scan(&answer)
+	if answer == 3 {
+
+		if slic[0] < slic[1] && slic[2] < slic[1] {
+
+			fmt.Println("*********************")
+
+			fmt.Println(slic[0], slic[2], slic[1])
+
+		} else if slic[2] < slic[0] && slic[0] < slic[1] {
+
+			fmt.Println("*********************")
+
+			fmt.Println(slic[2], slic[0], slic[1])
+
+		} else if slic[1] < slic[0] && slic[2] < slic[0] {
+
+			fmt.Println("*********************")
+
+			fmt.Println(slic[1], slic[2], slic[0])
+
+		} else if slic[2] < slic[1] && slic[1] < slic[0] {
+
+			fmt.Println("*********************")
+
+			fmt.Println(slic[2], slic[1], slic[0])
+
+		} else if slic[0] < slic[1] && slic[1] < slic[2] {
+
+			fmt.Println("*********************")
+
+			fmt.Println(slic[0], slic[1], slic[2])
+
+		} else if slic[1] < slic[0] && slic[0] < slic[2] {
+
+			fmt.Println("*********************")
+
+			fmt.Println(slic[1], slic[0], slic[2])
+
+		}
+
+	} else {
+
+		fmt.Println("*********************")
+
+		fmt.Println(arr)
+	}
+
+}
